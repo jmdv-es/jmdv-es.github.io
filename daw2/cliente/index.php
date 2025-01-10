2523d2ac4ccff5fa8854c2876c7dfd4120210d09
José Martínez <jmtzdv@gmail.com>
2025-01-10 20:10:40 +0100

    nuevo método de listar directorios con la api de github

--- daw2/cliente/index.php
+++ daw2/cliente/index.php
@@ -1,13 +1,18 @@
-<?php
-echo "Here are our files";
-$path = ".";
-$dh = opendir($path);
-$i=1;
-while (($file = readdir($dh)) !== false) {
-    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
-        echo "<a href='$path/$file'>$file</a><br /><br />";
-        $i++;
-    }
-}
-closedir($dh);
-?> 
+<html>
+  <body>
+    <script>
+      (async () => {
+        const response = await fetch('https://api.github.com/repos/jmdv-es/jmdv-es.github.io/contents/');
+        const data = await response.json();
+        let htmlString = '<ul>';
+        
+        for (let file of data) {
+          htmlString += `<li><a href="${file.path}">${file.name}</a></li>`;
+        }
+
+        htmlString += '</ul>';
+        document.getElementsByTagName('body')[0].innerHTML = htmlString;
+      })()
+    </script>
+  <body>
+</html>
